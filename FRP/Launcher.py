a5085763d033512c2029bcc6ac8832e6059cc192
R206658B <94453734+R206658B@users.noreply.github.com>
2024-06-17 12:21:59 +0200

    Update Launcher.py

--- FRP/Launcher.py
+++ FRP/Launcher.py
@@ -3,13 +3,13 @@ from datetime import datetime
 import pandas as pd
 
 st.set_page_config(
-    page_title="Attendance System", page_icon="C:\\Users\\Charles\\Desktop\\FRP\\coh_logo.png", layout="wide"
+    page_title="Attendance System", page_icon="coh_logo.png", layout="wide"
 )
 
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
-    st.image("C:\\Users\\Charles\\Desktop\\FRP\\coh_logo.png", width=155)
+    st.image("coh_logo.png", width=155)
 
 st.header(':rainbow[KANZURU ATTENDANCE SYSTEM!] :city_sunrise:', divider='rainbow')
-st.image("C:\\Users\\Charles\\Desktop\\FRP\\rowan_martin.jpg")
+st.image("rowan_martin.jpg")
 
